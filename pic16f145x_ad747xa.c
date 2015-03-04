6c31c165b3d6ca881bbb28454d40e5125de0c95d
Mathew Leising <mathew.leising@outlook.com>
2015-03-04 14:28:07 -0500

    Fixed copy/paste error
    Forgot to change copy paste of CS pins and made CS pins high on init

--- pic16f145x_ad747xa.c
+++ pic16f145x_ad747xa.c
@@ -3,6 +3,7 @@
 
 void ad747_init()
 {
+    CUR_CS = 1;
     VIB_CS = 1;
 }
 
@@ -10,13 +11,13 @@ unsigned int adc_8()
 {
     unsigned int data = 0x00;
 
-    VIB_CS = 0;
+    CUR_CS = 0;
 
     // OPCODE
     data = spi(0x00)<<4;
     data = data|(spi(0x00)>>4);
 
-    VIB_CS = 1;
+    CUR_CS = 1;
 
     return data;
 }
